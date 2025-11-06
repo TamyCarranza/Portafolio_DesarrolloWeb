2150725405abb9ef1142f3b57da95bc9edbb09eb
TamyCarranza <157515215+TamyCarranza@users.noreply.github.com>
2025-11-05 18:11:08 -0600

    Delete TiendaTamara directory

--- TiendaTamara/src/main/java/cr/tamara/tienda/service/CategoriaService.java
+++
@@ -1,8 +0,0 @@
-﻿package cr.tamara.tienda.service;
-
-import cr.tamara.tienda.domain.Categoria;
-import java.util.List;
-
-public interface CategoriaService {
-    List<Categoria> getCategorias();
-}
--- TiendaTamara/src/main/java/cr/tamara/tienda/service/ProductoService.java
+++
@@ -1,8 +0,0 @@
-﻿package cr.tamara.tienda.service;
-
-import cr.tamara.tienda.domain.Producto;
-import java.util.List;
-
-public interface ProductoService {
-    List<Producto> getProductos();
-}
